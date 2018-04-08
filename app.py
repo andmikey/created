c36a3067354ba5a592f17fa27c88e2a67fa9df24
Mikey <31766607+andmikey@users.noreply.github.com>
2018-04-08 16:45:11 +0100

    Add comments to code, neaten up

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
 import RPi.GPIO as g
-import time as t
+import time 
 import wikiquote
 import random
-import pyttsx3 as p
 import subprocess
 
+# Get the list of all quotes from Wikiquote
 quotes = wikiquote.quotes("Richard Stallman")
 
+# The button is plugged in on GPIO port 21
+# One wire at 21, one at GND
 g.setmode(g.BCM)
 g.setup(21, g.IN, pull_up_down = g.PUD_UP)
 
 try:
     while True:
+        # Poll state of button
         button_state = g.input(21)
+        
+        # Button has been pressed
         if button_state == False:
+            # Choose a random quote
             quote = random.choice(quotes)
+            # Speak the word of the lord
             subprocess.Popen(["espeak", "-v", "en-scottish", quote])
-            t.sleep(1)
+            # Wait for a second
+            time.sleep(1)
             
             
 
 except:
+    # Something unexpected happened; clean up after ourselves
     g.cleanup()
 
